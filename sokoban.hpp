e8c67a5d64bcfe199d8abfd3f22269185e281e2b
jand6 <jand6@kth.se>
2013-09-23 23:14:59 +0200

    Update sokoban.hpp

--- sokoban.hpp
+++ sokoban.hpp
@@ -12,7 +12,7 @@ struct State {
     State *parent;
 };
 
-std::vector<State> possibleSteps(std::vector<string> map, State current);
+std::vector<State> possibleSteps(std::vector<string> map, State *current);
 
 void parseBoard(std::vector<string> map, State root, std::vector<Point> &goal);
 
@@ -22,4 +22,4 @@ bool isGoal(std::vector<Point> goal, State state);
 
 State findPathTo(State start, Point goal);
 
-std::string getPath(State state);
+std::string getPath(State *state);

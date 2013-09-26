7b0c2e4453d8e612730841c3b2e03e8386aff757
Olli Niskanen <olli.niskanen@gmail.com>
2013-09-26 19:17:46 +0200

    Trying to fix node.cpp

--- node.cpp
+++ node.cpp
@@ -35,36 +35,6 @@ int heuristic(State state)
 	return value;
 }
 
-// // Smaller = closer to goal.
-// int heuristic(State state)
-// {
-// 	int value = 0;
-// 	int player_distance = 1000;
-
-// 	for (std::vector<Point>::iterator i = state.boxes.begin(); i != state.boxes.end(); ++i) {
-		
-// 		player_distance = min(player_distance,distance(*i,state.player)); // Distance from player to closest box
-		
-// 		//if (find(goals.begin(), goals.end(), *i) != goals.end())
-// 		//{
-// 		//	value -= 20;
-// 		//	continue;
-// 		//}
-// 		int minSoFar = 1000;
-// 		for (vector<Point>::iterator j = goals.begin(); j != goals.end(); ++j)
-// 		{
-// 			if (find(state.boxes.begin(), state.boxes.end(), *j) == state.boxes.end())
-// 			{
-// 				minSoFar = min(minSoFar, distance(*i, *j));
-// 			}
-// 		}
-// 		value += minSoFar;
-// 	}
-// 	value += 3*player_distance; // 3 arbitrarily chosen, needs some testing
-
-// 	return value;
-// }
-
 // As below, but only checks for either left-right or up-down. Assumes a box at given Point.
 bool Node::isBoxStuck(Point box, bool LRCheck) {
 
